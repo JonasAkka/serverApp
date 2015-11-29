a6eff335012a7046db00b0f557f77a415790f98c
JonasAkka <ak.younes.ak@gmail.com>
2015-11-29 14:30:40 +0000

    ..

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var todos = [{
 	id: 2,
 	description: "Go to Market",
 	completed: false
+}, {
+	id: 3,
+	description: "be riche",
+	completed: true
 }];
 
 
@@ -16,9 +20,25 @@ app.get('/', function (req, res) {
 	res.send('Todos API Root');
 });
 
+//GET /todos
 app.get('/todos', function (req, res) {
 	res.json(todos);
 	
+});
+
+//GET todos/:id
+app.get('/todos/:id', function (req, res) {
+	var todoId;
+	todos.forEach(function(value) {
+		if(value.id === parseInt(req.params.id, 10)) {
+			todoId = (value);
+		} 
+	});
+	if (todoId) {
+		res.json(todoId);
+	} else {
+		res.status(404).send("unable to have this model");
+	}
 });
 
 app.listen(PORT, function () {
