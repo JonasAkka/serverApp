54cbb246c8ee5844e262e19ed4d357e685260a76
JonasAkka <ak.younes.ak@gmail.com>
2015-12-02 12:05:42 +0000

    add for poduction

--- db.js
+++ db.js
@@ -1,8 +1,22 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect': 'sqlite',
-	'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var Sequelize;
+
+
+if (env === 'production') { //it's true if we are working on herku
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgres',
+
+	});
+
+} else {
+		 sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
+	});
+	
+}
+
 var db = {};
 db.todo = sequelize.import(__dirname + '/models/todo.js'); //the file called with import must have a specific format
 db.sequelize = sequelize;
